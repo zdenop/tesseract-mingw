c34aceb848f27d6b845e828fcdd1164a271bb6d7
zdenop <zdenop@gmail.com>
2012-02-08 22:25:33 +0100

    improved test file

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-This experimental test to compile tesseract 3.02 with mingw on Windows
-Library was created in Qt Creator+mingw with cmake.
+This experimental test to compile tesseract 3.02 with mingw on Windows.
+Library was created in Qt Creator+mingw with cmake (script is in cmake directory).
 
-Leptonica library is from [leptonica.com page](http://leptonica.com/source/vs2008-1.68.zip)
+Leptonica library(build by Microsoft Visual Studio 2008) is from [leptonica.com page](http://leptonica.com/source/vs2008-1.68.zip).
 
-Tesseract include files identified by [Tom Powers](http://groups.google.com/group/tesseract-dev/msg/a59952174f78c000)
+"Tesseract include files" was identified by [Tom Powers](http://groups.google.com/group/tesseract-dev/msg/a59952174f78c000) in tesserac 3.01 - maybe some file is missing.
 
 Nothing is guaranteed - use it on your own risk (or benefit ;-) )
 Improvements and forks are welcomed!
+
+Test
+====
+
+There is a test file (test_302.cpp) that show simple usage of library. If should be possible to compile with:
+
+    g++ -o bin/test_302 test_302.cpp -Iinclude/tesseract/ -Iinclude/leptonica/ -Llib -Llibs -ltesseract -llept168 -lws2_32
+    
+Than run:
+    bin\test_302 phototest.tif
--- test_302.cpp
+++ test_302.cpp
@@ -1,31 +1,62 @@
 /*
+ *
  * $ g++ -o bin/test_302 test_302.cpp -Iinclude/tesseract/ -Iinclude/leptonica/ -Llib -Llibs -ltesseract -llept168 -lws2_32
- * $ bin/test_302
-*/
+ * $ bin\test_302
+ */
 
 #include <baseapi.h>
 #include <allheaders.h>
 
 Pix *pix;
 
-int main(int argc, char *argv[]) {
-    tesseract::TessBaseAPI *myTess = new tesseract::TessBaseAPI();
-    printf("Tesseract-ocr version: %s\n",
-           tesseract::TessBaseAPI::Version());
-    printf("Leptonica version: %s\n",
-           getLeptonicaVersion());
-    
-    pix = pixRead("phototest.tif");
-    myTess->Init(NULL, "eng", tesseract::OEM_DEFAULT, NULL, 0, NULL, NULL, false);
-    myTess->SetImage(pix);
-    
-    char* outText = myTess->GetUTF8Text();
-    printf("OCR output:\n\n");
-    printf(outText);
+int main(int argc, char *argv[])
+{
+  tesseract::TessBaseAPI *myTess = new tesseract::TessBaseAPI();
 
-    myTess->Clear();
-    myTess->End();
-    delete [] outText;
-    pixDestroy(&pix);    
-    return 0;
+  printf("Tesseract-ocr version: %s\n",
+         tesseract::TessBaseAPI::Version());
+  printf("Leptonica version: %s\n",
+         getLeptonicaVersion());
+
+  if (argv[1] == NULL)
+    {
+      printf("Usage: tess_302 image_filename.\n");
+      exit(1);
+    }
+  // create pix
+  FILE *fp = fopen(argv[1], "r");
+  if (fp)
+    {
+      fclose(fp);
+      pix = pixRead(argv[1]);
+      if (pix == NULL)
+        {
+          printf("Unsupported image type.\n");
+          exit(3);
+        }
+      else
+        {
+          printf("Pix created\n");
+        }
+    }
+  else
+    {
+      printf("file '%s' doesnt exist.\n", argv[1]);
+      exit(3);
+    }
+
+  // initalize tesseract
+  myTess->Init(NULL, "eng", tesseract::OEM_DEFAULT, NULL, 0, NULL, NULL, false);
+
+  // run ocr
+  myTess->SetImage(pix);
+  char* outText = myTess->GetUTF8Text();
+  printf("OCR output:\n\n");
+  printf(outText);
+
+  myTess->Clear();
+  myTess->End();
+  delete [] outText;
+  pixDestroy(&pix);
+  return 0;
 }
