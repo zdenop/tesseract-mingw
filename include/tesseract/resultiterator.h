acad07deb966ae99a6342569662f2f23fb4e3a41
zdenop <zdenop@gmail.com>
2012-02-11 09:14:08 +0100

    update to 3.02 tesseract header files

--- include/tesseract/resultiterator.h
+++ include/tesseract/resultiterator.h
@@ -1,11 +1,12 @@
 ///////////////////////////////////////////////////////////////////////
 // File:        resultiterator.h
-// Description: Iterator for tesseract results that avoids using tesseract
-//              internal data structures.
-// Author:      Ray Smith
-// Created:     Fri Feb 26 11:01:06 PST 2010
+// Description: Iterator for tesseract results that is capable of
+//              iterating in proper reading order over Bi Directional
+//              (e.g. mixed Hebrew and English) text.
+// Author:      David Eger
+// Created:     Fri May 27 13:58:06 PST 2011
 //
-// (C) Copyright 2010, Google Inc.
+// (C) Copyright 2011, Google Inc.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -18,143 +19,180 @@
 //
 ///////////////////////////////////////////////////////////////////////
 
-#ifndef TESSERACT_API_RESULTITERATOR_H__
-#define TESSERACT_API_RESULTITERATOR_H__
+#ifndef TESSERACT_CCMAIN_RESULT_ITERATOR_H__
+#define TESSERACT_CCMAIN_RESULT_ITERATOR_H__
 
-#include "pageiterator.h"
+#include "ltrresultiterator.h"
+#include "genericvector.h"
 
 class BLOB_CHOICE_IT;
+class WERD_RES;
+class STRING;
 
 namespace tesseract {
 
 class Tesseract;
 
-// Class to iterate over tesseract results, providing access to all levels
-// of the page hierarchy, without including any tesseract headers or having
-// to handle any tesseract structures.
-// WARNING! This class points to data held within the TessBaseAPI class, and
-// therefore can only be used while the TessBaseAPI class still exists and
-// has not been subjected to a call of Init, SetImage, Recognize, Clear, End
-// DetectOS, or anything else that changes the internal PAGE_RES.
-// See apitypes.h for the definition of PageIteratorLevel.
-// See also base class PageIterator, which contains the bulk of the interface.
-// ResultIterator adds text-specific methods for access to OCR output.
-
-class ResultIterator : public PageIterator {
-  friend class ChoiceIterator;
+class ResultIterator : public LTRResultIterator {
  public:
-  // page_res and tesseract come directly from the BaseAPI.
-  // The rectangle parameters are copied indirectly from the Thresholder,
-  // via the BaseAPI. They represent the coordinates of some rectangle in an
-  // original image (in top-left-origin coordinates) and therefore the top-left
-  // needs to be added to any output boxes in order to specify coordinates
-  // in the original image. See TessBaseAPI::SetRectangle.
-  // The scale and scaled_yres are in case the Thresholder scaled the image
-  // rectangle prior to thresholding. Any coordinates in tesseract's image
-  // must be divided by scale before adding (rect_left, rect_top).
-  // The scaled_yres indicates the effective resolution of the binary image
-  // that tesseract has been given by the Thresholder.
-  // After the constructor, Begin has already been called.
-  ResultIterator(PAGE_RES* page_res, Tesseract* tesseract,
-                 int scale, int scaled_yres,
-                 int rect_left, int rect_top,
-                 int rect_width, int rect_height);
-  virtual ~ResultIterator();
+  static ResultIterator *StartOfParagraph(const LTRResultIterator &resit);
 
-  // ResultIterators may be copied! This makes it possible to iterate over
-  // all the objects at a lower level, while maintaining an iterator to
-  // objects at a higher level. These constructors DO NOT CALL Begin, so
-  // iterations will continue from the location of src.
-  // TODO: For now the copy constructor and operator= only need the base class
-  // versions, but if new data members are added, don't forget to add them!
+  // ResultIterator is copy constructible!
+  // The default copy constructor works just fine for us.
+  virtual ~ResultIterator() {}
 
   // ============= Moving around within the page ============.
+  // Moves the iterator to point to the start of the page to begin an iteration.
+  virtual void Begin();
 
-  // See PageIterator.
+  // Moves to the start of the next object at the given level in the
+  // page hierarchy in the appropriate reading order and returns false if
+  // the end of the page was reached.
+  // NOTE that RIL_SYMBOL will skip non-text blocks, but all other
+  // PageIteratorLevel level values will visit each non-text block once.
+  // Think of non text blocks as containing a single para, with a single line,
+  // with a single imaginary word.
+  // Calls to Next with different levels may be freely intermixed.
+  // This function iterates words in right-to-left scripts correctly, if
+  // the appropriate language has been loaded into Tesseract.
+  virtual bool Next(PageIteratorLevel level);
+
+  // IsAtBeginningOf() returns whether we're at the logical beginning of the
+  // given level.  (as opposed to ResultIterator's left-to-right top-to-bottom
+  // order).  Otherwise, this acts the same as PageIterator::IsAtBeginningOf().
+  // For a full description, see pageiterator.h
+  virtual bool IsAtBeginningOf(PageIteratorLevel level) const;
+
+  // Implement PageIterator's IsAtFinalElement correctly in a BiDi context.
+  // For instance, IsAtFinalElement(RIL_PARA, RIL_WORD) returns whether we
+  // point at the last word in a paragraph.  See PageIterator for full comment.
+  virtual bool IsAtFinalElement(PageIteratorLevel level,
+                                PageIteratorLevel element) const;
 
   // ============= Accessing data ==============.
 
   // Returns the null terminated UTF-8 encoded text string for the current
   // object at the given level. Use delete [] to free after use.
-  char* GetUTF8Text(PageIteratorLevel level) const;
+  virtual char* GetUTF8Text(PageIteratorLevel level) const;
 
-  // Returns the mean confidence of the current object at the given level.
-  // The number should be interpreted as a percent probability. (0.0f-100.0f)
-  float Confidence(PageIteratorLevel level) const;
+  // Return whether the current paragraph's dominant reading direction
+  // is left-to-right (as opposed to right-to-left).
+  bool ParagraphIsLtr() const;
 
-  // ============= Functions that refer to words only ============.
+  // ============= Exposed only for testing =============.
 
-  // Returns the font attributes of the current word. If iterating at a higher
-  // level object than words, eg textlines, then this will return the
-  // attributes of the first word in that textline.
-  // The actual return value is a string representing a font name. It points
-  // to an internal table and SHOULD NOT BE DELETED. Lifespan is the same as
-  // the iterator itself, ie rendered invalid by various members of
-  // TessBaseAPI, including Init, SetImage, End or deleting the TessBaseAPI.
-  // Pointsize is returned in printers points (1/72 inch.)
-  const char* WordFontAttributes(bool* is_bold,
-                                 bool* is_italic,
-                                 bool* is_underlined,
-                                 bool* is_monospace,
-                                 bool* is_serif,
-                                 bool* is_smallcaps,
-                                 int* pointsize,
-                                 int* font_id) const;
+  // Yields the reading order as a sequence of indices and (optional)
+  // meta-marks for a set of words (given left-to-right).
+  // The meta marks are passed as negative values:
+  //   kMinorRunStart  Start of minor direction text.
+  //   kMinorRunEnd    End of minor direction text.
+  //   kComplexWord    The next indexed word contains both left-to-right and
+  //                    right-to-left characters and was treated as neutral.
+  //
+  // For example, suppose we have five words in a text line,
+  // indexed [0,1,2,3,4] from the leftmost side of the text line.
+  // The following are all believable reading_orders:
+  //
+  // Left-to-Right (in ltr paragraph):
+  //     { 0, 1, 2, 3, 4 }
+  // Left-to-Right (in rtl paragraph):
+  //     { kMinorRunStart, 0, 1, 2, 3, 4, kMinorRunEnd }
+  // Right-to-Left (in rtl paragraph):
+  //     { 4, 3, 2, 1, 0 }
+  // Left-to-Right except for an RTL phrase in words 2, 3 in an ltr paragraph:
+  //     { 0, 1, kMinorRunStart, 3, 2, kMinorRunEnd, 4 }
+  static void CalculateTextlineOrder(
+      bool paragraph_is_ltr,
+      const GenericVector<StrongScriptDirection> &word_dirs,
+      GenericVectorEqEq<int> *reading_order);
 
-  // Returns true if the current word was found in a dictionary.
-  bool WordIsFromDictionary() const;
+  static const int kMinorRunStart;
+  static const int kMinorRunEnd;
+  static const int kComplexWord;
 
-  // Returns true if the current word is numeric.
-  bool WordIsNumeric() const;
-
-  // ============= Functions that refer to symbols only ============.
-
-  // Returns true if the current symbol is a superscript.
-  // If iterating at a higher level object than symbols, eg words, then
-  // this will return the attributes of the first symbol in that word.
-  bool SymbolIsSuperscript() const;
-  // Returns true if the current symbol is a subscript.
-  // If iterating at a higher level object than symbols, eg words, then
-  // this will return the attributes of the first symbol in that word.
-  bool SymbolIsSubscript() const;
-  // Returns true if the current symbol is a dropcap.
-  // If iterating at a higher level object than symbols, eg words, then
-  // this will return the attributes of the first symbol in that word.
-  bool SymbolIsDropcap() const;
-};
-
-// Class to iterate over the classifier choices for a single RIL_SYMBOL.
-class ChoiceIterator {
- public:
-  // Construction is from a ResultIterator that points to the symbol of
-  // interest. The ChoiceIterator allows a one-shot iteration over the
-  // choices for this symbol and after that is is useless.
-  explicit ChoiceIterator(const ResultIterator& result_it);
-  ~ChoiceIterator();
-
-  // Moves to the next choice for the symbol and returns false if there
-  // are none left.
-  bool Next();
-
-  // ============= Accessing data ==============.
-
-  // Returns the null terminated UTF-8 encoded text string for the current
-  // choice.
-  // NOTE: Unlike ResultIterator::GetUTF8Text, the return points to an
-  // internal structure and should NOT be delete[]ed to free after use.
-  const char* GetUTF8Text() const;
-
-  // Returns the confidence of the current choice.
-  // The number should be interpreted as a percent probability. (0.0f-100.0f)
-  float Confidence() const;
+ protected:
+  // We presume the data associated with the given iterator will outlive us.
+  // NB: This is private because it does something that is non-obvious:
+  //   it resets to the beginning of the paragraph instead of staying wherever
+  //   resit might have pointed.
+  explicit ResultIterator(const LTRResultIterator &resit);
 
  private:
-  // Pointer to the Tesseract object owned by the API.
-  Tesseract* tesseract_;
-  // Iterator over the blob choices.
-  BLOB_CHOICE_IT* choice_it_;
+  // Calculates the current paragraph's dominant writing direction.
+  // Typically, members should use current_paragraph_ltr_ instead.
+  bool CurrentParagraphIsLtr() const;
+
+  // Returns word indices as measured from resit->RestartRow() = index 0
+  // for the reading order of words within a textline given an iterator
+  // into the middle of the text line.
+  // In addition to non-negative word indices, the following negative values
+  // may be inserted:
+  //   kMinorRunStart  Start of minor direction text.
+  //   kMinorRunEnd    End of minor direction text.
+  //   kComplexWord    The previous word contains both left-to-right and
+  //                   right-to-left characters and was treated as neutral.
+  void CalculateTextlineOrder(bool paragraph_is_ltr,
+                              const LTRResultIterator &resit,
+                              GenericVectorEqEq<int> *indices) const;
+  // Same as above, but the caller's ssd gets filled in if ssd != NULL.
+  void CalculateTextlineOrder(bool paragraph_is_ltr,
+                              const LTRResultIterator &resit,
+                              GenericVector<StrongScriptDirection> *ssd,
+                              GenericVectorEqEq<int> *indices) const;
+
+  // What is the index of the current word in a strict left-to-right reading
+  // of the row?
+  int LTRWordIndex() const;
+
+  // Given an iterator pointing at a word, returns the logical reading order
+  // of blob indices for the word.
+  void CalculateBlobOrder(GenericVector<int> *blob_indices) const;
+
+  // Precondition: current_paragraph_is_ltr_ is set.
+  void MoveToLogicalStartOfTextline();
+
+  // Precondition: current_paragraph_is_ltr_ and in_minor_direction_ are set.
+  void MoveToLogicalStartOfWord();
+
+  // Are we pointing at the final (reading order) symbol of the word?
+  bool IsAtFinalSymbolOfWord() const;
+
+  // Are we pointing at the first (reading order) symbol of the word?
+  bool IsAtFirstSymbolOfWord() const;
+
+  // Append any extra marks that should be appended to this word when printed.
+  // Mostly, these are Unicode BiDi control characters.
+  void AppendSuffixMarks(STRING *text) const;
+
+  // Appends the current word in reading order to the given buffer.
+  void AppendUTF8WordText(STRING *text) const;
+
+  // Appends the text of the current text line, *assuming this iterator is
+  // positioned at the beginning of the text line*  This function
+  // updates the iterator to point to the first position past the text line.
+  // Each textline is terminated in a single newline character.
+  // If the textline ends a paragraph, it gets a second terminal newline.
+  void IterateAndAppendUTF8TextlineText(STRING *text);
+
+  // Appends the text of the current paragraph in reading order
+  // to the given buffer.
+  // Each textline is terminated in a single newline character, and the
+  // paragraph gets an extra newline at the end.
+  void AppendUTF8ParagraphText(STRING *text) const;
+
+  // Returns whether the bidi_debug flag is set to at least min_level.
+  bool BidiDebug(int min_level) const;
+
+  bool current_paragraph_is_ltr_;
+
+  // Is the currently pointed-at character at the beginning of
+  // a minor-direction run?
+  bool at_beginning_of_minor_run_;
+
+  // Is the currently pointed-at character in a minor-direction sequence?
+  bool in_minor_direction_;
 };
 
 }  // namespace tesseract.
 
-#endif  // TESSERACT_API_RESULT_ITERATOR_H__
+#endif  // TESSERACT_CCMAIN_RESULT_ITERATOR_H__
